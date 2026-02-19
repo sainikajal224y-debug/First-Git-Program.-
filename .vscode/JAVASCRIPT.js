0aaa7aea713099ac8dad25feee40716cc4969fbb
sainikajal224y-debug <sainikajal224y@gmail.com>
2026-02-19 19:35:15 +0530

    changing modes

--- .vscode/JAVASCRIPT.js
+++ .vscode/JAVASCRIPT.js
@@ -1,2 +1,16 @@
-prompt("Hello World!");
- 
+let modeBtn = document.querySelector("#modeBtn");
+let body = document.querySelector("body");
+let currMode = "light";
+
+modeBtn.addEventListener("click", () =>{
+    if(currMode === "light"){
+        currMode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    } else {
+        currMode = "light";
+         body.classList.add("light")
+         body.classList.remove("dark");
+    }
+    console.log(currMode);
+});
